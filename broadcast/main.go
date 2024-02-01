6d8d88c43bd88d3a33de5799842048526f64a2fc
timlock <tim-lock@hotmail.de>
2024-02-01 17:56:09 +0100

    Fix 3b Multi-Node Broadcast

--- broadcast/main.go
+++ broadcast/main.go
@@ -2,41 +2,74 @@ package main
 
 import (
 	"encoding/json"
-	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"log"
 	"sync"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-type Broadcast struct {
-	messages      []int
-	versionVector map[string]int
+// type History struct {
+// 	sync.RWMutex
+// 	inner map[string]map[int]bool
+// }
+
+// func (m *History) AddNode(id string) {
+// 	m.RLock()
+// 	_, ok := m.inner[id]
+// 	m.RUnlock()
+// 	if !ok {
+// 		m.Lock()
+// 		m.inner[id] = make(map[int]bool)
+// 		m.Unlock()
+// 	}
+// }
+// func (m *History) AddMsgID(id string, msgID int) {
+// 	m.Lock()
+// 	m.inner[id][msgID] = true
+// 	m.Unlock()
+// }
+// func (m *History) Contains(id string, msgID int) bool {
+// 	m.RLock()
+// 	defer m.RUnlock()
+// 	_, ok := m.inner[id][msgID]
+// 	return ok
+// }
+
+// type Message struct {
+// 	Content int
+// 	Src     string
+// }
+
+type MessageStore struct {
 	sync.RWMutex
+	messages    []int
+	vectorClock map[string]int
 }
 
-func (b *Broadcast) addNode(id string) {
+func (b *MessageStore) AddNode(id string) {
 	b.RLock()
-	_, ok := b.versionVector[id]
+	_, ok := b.vectorClock[id]
 	b.RUnlock()
 	if !ok {
 		b.Lock()
-		b.versionVector[id] = 0
+		b.vectorClock[id] = 0
 		b.Unlock()
 	}
 }
-func (b *Broadcast) updateNode(id string, time int) {
+func (b *MessageStore) UpdateNode(src string, time int) {
 	b.Lock()
-	b.versionVector[id] = time
+	b.vectorClock[src] = time
 	b.Unlock()
 }
 
-func (b *Broadcast) addMessage(id string, message int) {
+func (b *MessageStore) AddMessage(src string, content int) {
 	b.Lock()
-	b.messages = append(b.messages, message)
+	b.messages = append(b.messages, content)
 	b.Unlock()
 }
-func (b *Broadcast) read(id string) []int {
+func (b *MessageStore) read(id string) []int {
 	b.RLock()
-	lastRead := b.versionVector[id]
+	lastRead := b.vectorClock[id]
 	amount := len(b.messages)
 	if lastRead < amount {
 		messages := make([]int, amount-lastRead)
@@ -44,7 +77,7 @@ func (b *Broadcast) read(id string) []int {
 			messages = append(messages, b.messages[i])
 		}
 		b.RUnlock()
-		b.updateNode(id, amount)
+		b.UpdateNode(id, amount)
 		return messages
 	} else {
 		b.RUnlock()
@@ -52,60 +85,93 @@ func (b *Broadcast) read(id string) []int {
 	}
 }
 
+func (b *MessageStore) ReadAll() []int {
+	b.RLock()
+	defer b.RUnlock()
+	messages := make([]int, len(b.messages))
+	messages = append(messages, b.messages...)
+	// for _, msg := range b.messages {
+	// 	messages = append(messages, msg)
+	// }
+	return messages
+}
+
+type Server struct {
+	node  *maelstrom.Node
+	store *MessageStore
+}
+
+func (s *Server) handleBroadcast(msg maelstrom.Message) error {
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+	message := int(body["message"].(float64))
+	s.store.AddMessage(msg.Src, message)
+	payload := make(map[string]any)
+	payload["type"] = "replicate"
+	payload["message"] = message
+	go s.sendBroadcast(payload)
+	return s.node.Reply(msg, map[string]any{
+		"type": "broadcast_ok",
+	})
+}
+func (s *Server) sendBroadcast(payload map[string]any) {
+	for _, dest := range s.node.NodeIDs() {
+		if dest != s.node.ID() {
+			s.node.RPC(dest, payload, func(msg maelstrom.Message) error { return nil })
+		}
+	}
+
+}
+func (s *Server) handleRead(msg maelstrom.Message) error {
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+	messages := s.store.ReadAll()
+	return s.node.Reply(msg, map[string]any{
+		"type":     "read_ok",
+		"messages": messages,
+	})
+}
+func (s *Server) handleTopology(msg maelstrom.Message) error {
+	s.store.AddNode(msg.Src)
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+	for _, other := range s.node.NodeIDs() {
+		if other != s.node.ID() {
+			s.store.AddNode(other)
+		}
+	}
+	return s.node.Reply(msg, map[string]any{
+		"type": "topology_ok",
+	})
+}
+
+func (s *Server) handleReplicate(msg maelstrom.Message) error {
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+	message := int(body["message"].(float64))
+	s.store.AddMessage(msg.Src, message)
+	return s.node.Reply(msg, map[string]any{
+		"type": "replicate_ok",
+	})
+}
 func main() {
 	n := maelstrom.NewNode()
-	broadcast := Broadcast{messages: make([]int, 20*10), versionVector: make(map[string]int)}
-	n.Handle("init", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-		for _, other := range n.NodeIDs(){
-			if other != msg.Dest{
-				broadcast.addNode(other)
-			}
-		}
-		return n.Reply(msg, map[string]any{
-			"type":        "init_ok",
-			"in_reply_to": 1,
-		})
-	})
-	n.Handle("broadcast", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-		message := int(body["message"].(float64))
-		broadcast.addMessage(msg.Src, message)
-		for _, other := range n.NodeIDs() {
-			if other != msg.Dest {
-				n.Send(other, body)
-			}
-		}
-		return n.Reply(msg, map[string]any{
-			"type": "broadcast_ok",
-		})
-	})
-	n.Handle("read", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-		messages := broadcast.read(msg.Src)
-		return n.Reply(msg, map[string]any{
-			"type":     "read_ok",
-			"messages": messages,
-		})
-	})
-	n.Handle("topology", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-		return n.Reply(msg, map[string]any{
-			"type": "topology_ok",
-		})
-	})
+	broadcast := MessageStore{messages: make([]int, 0), vectorClock: make(map[string]int)}
+	// history := History{inner: make(map[string]map[int]bool)}
+	server := Server{node: n, store: &broadcast}
+
+	n.Handle("broadcast", server.handleBroadcast)
+	n.Handle("read", server.handleRead)
+	n.Handle("topology", server.handleTopology)
+	n.Handle("replicate", server.handleReplicate)
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
